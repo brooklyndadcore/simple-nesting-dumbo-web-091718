44a25ef1ee6fa59b94f1fec8385e48dfc17bf54b
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-20 17:16:11 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -61,6 +61,10 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
+  programmer_hash[:yukihiro_matsumoto] = {
+  :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+}
 
 
 	programmer_hash = 
@@ -79,10 +83,6 @@ def adding_matz
         }
      }
 
-  programmer_hash[:yukihiro_matsumoto] = {
-  :known_for => "Ruby",
-    :languages => ["LISP", "C"]
-}
 
 return programmer_hash[:yukihiro_matsumoto]
     
